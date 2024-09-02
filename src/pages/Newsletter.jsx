e07a580f1d827df7420ea66c805c07a583f36afa
akolle <andykoller55@yahoo.com>
2024-09-02 12:00:37 -0400

    handle errors

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -8,10 +8,16 @@ export const action = async ({ request }) => {
   const formData = await request.formData()
   const data = Object.fromEntries(formData)
 
-  const response = await axios.post(newsletterUrl, data)
-  console.log(response)
-  toast.success(response.data.msg)
-  return redirect('/')
+  try {
+    const response = await axios.post(newsletterUrl, data)
+    console.log(response)
+    toast.success(response.data.msg)
+    return redirect('/')
+  } catch (error) {
+    console.log(error)
+    toast.error(error?.response?.data?.msg) //optional chaining in case error format is different
+    return error
+  }
 }
 
 const Newsletter = () => {
@@ -30,7 +36,7 @@ const Newsletter = () => {
           className="form-input"
           name="name"
           id="name"
-          defaultValue="john"
+          required
         />
       </div>
       {/* {lastName} */}
@@ -43,7 +49,7 @@ const Newsletter = () => {
           className="form-input"
           name="lastName"
           id="lastName"
-          defaultValue="smith"
+          required
         />
       </div>
       {/* {email} */}
@@ -57,6 +63,7 @@ const Newsletter = () => {
           name="email"
           id="email"
           defaultValue="test@test.com"
+          required
         />
       </div>
       <button
