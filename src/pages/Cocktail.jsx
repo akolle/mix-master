3660b77f0310b183e53e1e85f9f72a0e73ae9d19
akolle <andykoller55@yahoo.com>
2024-09-02 13:01:31 -0400

    react query single cocktail page

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -5,17 +5,30 @@ import Wrapper from '../assets/wrappers/CocktailPage'
 const singleCocktailUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
-export const loader = async ({ params }) => {
-  const { id } = params
-  const { data } = await axios.get(`${singleCocktailUrl}${id}`)
+import { useQuery } from '@tanstack/react-query'
 
-  return { id, data }
+const singleCocktailQuery = (id) => {
+  return {
+    queryKey: ['cocktail', id],
+    queryFn: async () => {
+      const { data } = await axios.get(`${singleCocktailUrl}${id}`)
+      return data
+    },
+  }
 }
 
-const Cocktail = () => {
-  const { id, data } = useLoaderData()
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params
+    await queryClient.ensureQueryData(singleCocktailQuery(id))
+    return { id }
+  }
 
-  // if (!data) return <h2>something went wrong...</h2>
+const Cocktail = () => {
+  const { id } = useLoaderData()
+
+  const { data } = useQuery(singleCocktailQuery(id))
   if (!data) return <Navigate to="/" />
 
   const singleDrink = data.drinks[0]
